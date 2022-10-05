889c22812255292b278319e827ea9b324777f7f8
dengyecode <108976390+dengyecode@users.noreply.github.com>
2022-10-05 14:41:32 +0800

    Update base_options.py

--- options/base_options.py
+++ options/base_options.py
@@ -29,8 +29,8 @@ class BaseOptions():
         parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the image for data augmentation')
         parser.add_argument('--no_rotation', action='store_true', help='if specified, do not rotation for data augmentation')
         parser.add_argument('--no_augment', action='store_true', help='if specified, do not augment the image for data augmentation')
-        parser.add_argument('--batchSize', type=int, default=1, help='input batch size')
-        parser.add_argument('--nThreads', type=int, default=1, help='# threads for loading data')
+        parser.add_argument('--batchSize', type=int, default=6, help='input batch size')
+        parser.add_argument('--nThreads', type=int, default=6, help='# threads for loading data')
         parser.add_argument('--no_shuffle', action='store_true',help='if true, takes images serial')
 
         # display parameter define
